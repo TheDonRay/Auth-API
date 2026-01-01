3108e87317ebd0af1976e2e494c7630f040d5b30
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-01 15:51:54 -0500

    added some key notes

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,4 +1,4 @@
-const posts = require('')
+const posts = require('../data/practicedata.js'); 
 
 const authFunction = (req, res) => { 
     res.json(posts); 
--- backend/controllers/jwtcontroller.js
+++ backend/controllers/jwtcontroller.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken'); 
-const posts = require('./authcontroller.js'); 
+const posts = require('../data/practicedata.js'); 
 
 const jwtFunction = (req, res) => { 
     //first lets get the user name here 
     const username = req.body.username; 
-    const user = { name: username }; 
+    const userExists = posts.find(post => post.username == username);  
+    if (userExists) { 
+        const user = { name:username }; 
     //to create a JWT webtoken we can pass in the jwt.sign and the first thing it takes is the payload 
-    // the next thing we need is our secret key  
-    jwt.sign(user, process.env.SECRET_KEY)
+    // the next thing we need is our secret key. To create a secret value you can use the crypt library inside nodeJs
+    jwt.sign(user, process.env.SECRET_KEY) 
+    }
     
 
 }
